e929177de136bc23b0d545a59d06d96602b49068
DarKRs <centralstreet_24@mail.ru>
2024-10-18 09:48:17 +0600

    Слой Infrastructure. DbContext и репозиторий task

---
+++ TimeFlow.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeFlow.Domain.Entities;
+
+namespace TimeFlow.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ TimeFlow.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeFlow.Application.Interfaces;
+using TimeFlow.Domain.Entities;
+using TimeFlow.Infrastructure.Data;
+
+namespace TimeFlow.Infrastructure.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllTasksAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task<TaskItem> GetTaskByIdAsync(int id)
+        {
+            return await _context.Tasks.FindAsync(id);
+        }
+
+        public async Task AddTaskAsync(TaskItem task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateTaskAsync(TaskItem task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteTaskAsync(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task != null)
+            {
+                _context.Tasks.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}

499d39800c45dccfde87c5c9a112177063ec0ecd
DarKRs <centralstreet_24@mail.ru>
2024-10-18 09:44:42 +0600

    Слой Application

---
+++ TimeFlow.Application/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using TimeFlow.Domain.Entities;
+
+namespace TimeFlow.Application.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllTasksAsync();
+        Task<TaskItem> GetTaskByIdAsync(int id);
+        Task AddTaskAsync(TaskItem task);
+        Task UpdateTaskAsync(TaskItem task);
+        Task DeleteTaskAsync(int id);
+    }
+}
---
+++ TimeFlow.Application/Services.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeFlow.Application.Interfaces;
+using TimeFlow.Domain.Entities;
+
+namespace TimeFlow.Application
+{
+    public class TaskService
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskService(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetTasksAsync()
+        {
+            return await _taskRepository.GetAllTasksAsync();
+        }
+
+        public async Task<TaskItem> GetTaskByIdAsync(int id)
+        {
+            return await _taskRepository.GetTaskByIdAsync(id);
+        }
+
+        public async Task AddTaskAsync(TaskItem task)
+        {
+            await _taskRepository.AddTaskAsync(task);
+        }
+
+        public async Task UpdateTaskAsync(TaskItem task)
+        {
+            await _taskRepository.UpdateTaskAsync(task);
+        }
+
+        public async Task DeleteTaskAsync(int id)
+        {
+            await _taskRepository.DeleteTaskAsync(id);
+        }
+    }
+}

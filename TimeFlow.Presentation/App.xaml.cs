59271cb7dd7187c89d605330ccaac8ae947ddace
DarKRs <centralstreet_24@mail.ru>
2024-10-18 09:21:32 +0600

    Добавьте файлы проекта.

---
+++ TimeFlow.Presentation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeFlow.Presentation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TimeFlow.Presentation/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeFlow.Presentation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

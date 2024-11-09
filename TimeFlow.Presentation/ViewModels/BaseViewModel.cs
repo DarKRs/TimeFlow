0676359a9aa5ea08c0625437e1e120a2237fb69c
DarKRs <centralstreet_24@mail.ru>
2024-11-09 14:38:49 +0600

    SetProperty

--- TimeFlow.Presentation/ViewModels/BaseViewModel.cs
+++ TimeFlow.Presentation/ViewModels/BaseViewModel.cs
@@ -16,5 +16,15 @@ namespace TimeFlow.Presentation.ViewModels
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }

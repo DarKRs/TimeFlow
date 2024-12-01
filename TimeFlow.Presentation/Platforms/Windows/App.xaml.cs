9a6bad31133144ec563293cd1fdd4d6e32ceee47
DarKRs <centralstreet_24@mail.ru>
2024-12-01 13:38:08 +0600

    Открытие на весь экран

--- TimeFlow.Presentation/Platforms/Windows/App.xaml.cs
+++ TimeFlow.Presentation/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,6 +22,43 @@ namespace TimeFlow.Presentation.WinUI
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            base.OnLaunched(args);
+
+            // Получаем текущее окно приложения
+            var mauiWindow = Application.Windows.FirstOrDefault()?.Handler?.PlatformView as Microsoft.UI.Xaml.Window;
+
+            if (mauiWindow != null)
+            {
+                var appWindow = GetAppWindowForCurrentWindow(mauiWindow);
+                if (appWindow != null)
+                {
+                    // Создаём презентер для окна
+                    var presenter = appWindow.Presenter as OverlappedPresenter;
+                    if (presenter != null)
+                    {
+                        // Разрешаем изменение размера и убираем режим полного экрана
+                        presenter.IsResizable = true;
+                        presenter.IsMaximizable = true;
+                        presenter.SetBorderAndTitleBar(true, true);
+                    }
+
+                    // Устанавливаем размеры окна на весь экран
+                    var displayArea = DisplayArea.GetFromWindowId(appWindow.Id, DisplayAreaFallback.Primary);
+                    var workArea = displayArea.WorkArea;
+                    appWindow.MoveAndResize(new RectInt32(workArea.X, workArea.Y, workArea.Width, workArea.Height));
+                }
+            }
+        }
+
+        private AppWindow? GetAppWindowForCurrentWindow(Microsoft.UI.Xaml.Window mauiWindow)
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(mauiWindow);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(windowId);
+        }
     }
 
 }

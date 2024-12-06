d31148662113a88e8cb601100b1448d847d10e34
DarKRs <centralstreet_24@mail.ru>
2024-12-06 14:23:55 +0600

    Модель привычки

---
+++ TimeFlow.Domain/Entities/Habit.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeFlow.Domain.Entities
+{
+    public class Habit
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<HabitStage> Stages { get; set; }
+
+        // Периодичность выполнения
+        public HabitPeriodicity Periodicity { get; set; }
+
+        // Записи выполнения
+        public ICollection<HabitRecord> CompletionRecords { get; set; }
+
+        // Цепочка выполнения
+        public int CurrentStreak { get; set; } = 0;
+        public int LongestStreak { get; set; } = 0;
+        public DateTime? LastCompletionDate { get; set; }
+
+        public int AllowedMissedDays { get; set; } = 1;
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+    }
+
+    public class HabitStage
+    {
+        public int Id { get; set; }
+        public int HabitId { get; set; }
+        public Habit Habit { get; set; }
+
+        public string Description { get; set; }
+        public CompletionStatus Status { get; set; }
+        public int Order { get; set; }
+    }
+
+    public class HabitPeriodicity
+    {
+        public int Id { get; set; }
+        public int HabitId { get; set; }
+        public Habit Habit { get; set; }
+        public Frequency FrequencyType { get; set; }
+        public string DaysOfWeek { get; set; }
+    }
+
+    public class HabitRecord
+    {
+        public int Id { get; set; }
+        public int HabitId { get; set; }
+        public Habit Habit { get; set; }
+
+        public DateTime Date { get; set; }
+        public CompletionStatus Status { get; set; }
+    }
+
+    public enum Frequency
+    {
+        Daily,
+        Weekly,
+        Custom
+    }
+
+    public enum CompletionStatus
+    {
+        NotDone,
+        PartiallyDone,
+        Done
+    }
+}
--- TimeFlow.Infrastructure/Data/AppDbContext.cs
+++ TimeFlow.Infrastructure/Data/AppDbContext.cs
@@ -7,6 +7,11 @@ namespace TimeFlow.Infrastructure.Data
     {
         public DbSet<TaskItem> Tasks { get; set; }
         public DbSet<PomodoroSession> PomodoroSessions { get; set; }
+        //Привычки
+        public DbSet<Habit> Habits { get; set; }
+        public DbSet<HabitStage> HabitStages { get; set; }
+        public DbSet<HabitPeriodicity> Periodicities { get; set; }
+        public DbSet<HabitRecord> CompletionRecords { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
@@ -17,6 +22,20 @@ namespace TimeFlow.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
+            modelBuilder.Entity<HabitStage>()
+                .HasOne(hs => hs.Habit)
+                .WithMany(h => h.Stages)
+                .HasForeignKey(hs => hs.HabitId);
+
+            modelBuilder.Entity<HabitPeriodicity>()
+                .HasOne(p => p.Habit)
+                .WithOne(h => h.Periodicity)
+                .HasForeignKey<HabitPeriodicity>(p => p.HabitId);
+
+            modelBuilder.Entity<HabitRecord>()
+                .HasOne(cr => cr.Habit)
+                .WithMany(h => h.CompletionRecords)
+                .HasForeignKey(cr => cr.HabitId);
         }
     }
 }

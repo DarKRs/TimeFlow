54cdeedbfd8adddf72b73a42d05cda590a17e76a
DarKRs <centralstreet_24@mail.ru>
2024-12-07 21:35:20 +0600

    Панель дат в трекере привычек

--- TimeFlow.Presentation/Utils/Converters.cs
+++ TimeFlow.Presentation/Utils/Converters.cs
@@ -92,4 +92,21 @@ namespace TimeFlow.Presentation.Utils
         }
     }
 
+    public class DateToDayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date)
+            {
+                return date.ToString("dd"); 
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
--- TimeFlow.Presentation/ViewModels/HabitTrackerViewModel.cs
+++ TimeFlow.Presentation/ViewModels/HabitTrackerViewModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Windows.Input;
 using TimeFlow.Core.Interfaces;
 using TimeFlow.Domain.Entities;
@@ -14,37 +10,152 @@ namespace TimeFlow.Presentation.ViewModels
     {
         private readonly IHabitService _habitService;
 
-        public ObservableCollection<Habit> Habits { get; set; }
+        private DateTime _currentMonth;
+        public string CurrentMonthYear
+        {
+            get
+            {
+                var cultureInfo = CultureInfo.GetCultureInfo("ru-RU");
+                return cultureInfo.TextInfo.ToTitleCase(_currentMonth.ToString("MMMM yyyy", cultureInfo));
+            }
+        }
 
-        public ICommand UpdateHabitStatusCommand { get; }
+        public ObservableCollection<Habit> Habits { get; set; } = new();
+
+        public DateTime OldestDate { get; private set; }
+        public DateTime NewestDate { get; private set; }
+
+        public List<DateTime> CurrentMonthDates
+        {
+            get
+            {
+                var maxDate = DateTime.Today; // Текущая дата как крайняя правая
+                var minDate = maxDate.AddDays(-31); // 31 день назад
+                return Enumerable.Range(0, (maxDate - minDate).Days + 1)
+                    .Select(offset => minDate.AddDays(offset))
+                    .ToList();
+            }
+        }
+
+        public ICommand PreviousMonthCommand { get; }
+        public ICommand NextMonthCommand { get; }
 
         public HabitTrackerViewModel(IHabitService habitService)
         {
             _habitService = habitService;
-            Habits = new ObservableCollection<Habit>();
+            _currentMonth = DateTime.Now;
 
-            UpdateHabitStatusCommand = new Command<Habit>(async (habit) => await UpdateHabitStatus(habit));
+            PreviousMonthCommand = new Command(() => ChangeMonth(-1), () => CanNavigateToPreviousMonth);
+            NextMonthCommand = new Command(() => ChangeMonth(1), () => CanNavigateToNextMonth);
 
             LoadHabits();
+            UpdateDateLimits();
+        }
+
+        private void ChangeMonth(int offset)
+        {
+            _currentMonth = _currentMonth.AddMonths(offset);
+            OnPropertyChanged(nameof(CurrentMonthYear));
+            LoadHabits();
+        }
+
+        private bool CanNavigateToPreviousMonth => _currentMonth > OldestDate;
+        private bool CanNavigateToNextMonth => _currentMonth < NewestDate;
+
+        private void UpdateCommandStates()
+        {
+            ((Command)PreviousMonthCommand).ChangeCanExecute();
+            ((Command)NextMonthCommand).ChangeCanExecute();
+        }
+
+        private void UpdateDateLimits()
+        {
+            if (Habits.Any())
+            {
+                OldestDate = Habits.Min(h => h.CreatedDate).AddMonths(-1); 
+                NewestDate = DateTime.Today.AddMonths(1); 
+            }
+            else
+            {
+                OldestDate = DateTime.Today;
+                NewestDate = DateTime.Today;
+            }
         }
 
         private async void LoadHabits()
         {
-            var habits = await _habitService.GetAllHabitsAsync();
+            var habits = await _habitService.GetHabitsForMonth(_currentMonth.Year, _currentMonth.Month);
             Habits.Clear();
             foreach (var habit in habits)
             {
                 Habits.Add(habit);
             }
-        }
 
-        private async Task UpdateHabitStatus(Habit habit)
-        {
-            if (habit != null)
+            //Тестовые привычки
+            Habits.Add(new Habit
             {
-                await _habitService.UpdateHabitAsync(habit);
-                LoadHabits(); 
-            }
+                Id = 1,
+                Name = "Пить воду",
+                Description = "Выпивать 8 стаканов воды в день",
+                Periodicity = new HabitPeriodicity
+                {
+                    FrequencyType = Frequency.Daily,
+                },
+                CompletionRecords = new List<HabitRecord>
+                {
+                    new HabitRecord { Date = DateTime.Today.AddDays(-2), Status = CompletionStatus.Done },
+                    new HabitRecord { Date = DateTime.Today.AddDays(-1), Status = CompletionStatus.PartiallyDone },
+                    new HabitRecord { Date = DateTime.Today, Status = CompletionStatus.NotDone }
+                },
+                CurrentStreak = 2,
+                LongestStreak = 5,
+                LastCompletionDate = DateTime.Today.AddDays(-1),
+                CreatedDate = DateTime.Today.AddDays(-30)
+            });
+
+            Habits.Add(new Habit
+            {
+                Id = 2,
+                Name = "Чтение книг",
+                Description = "Читать 10 страниц книги каждый день",
+                Periodicity = new HabitPeriodicity
+                {
+                    FrequencyType = Frequency.Daily,
+                },
+                CompletionRecords = new List<HabitRecord>
+                {
+                    new HabitRecord { Date = DateTime.Today.AddDays(-3), Status = CompletionStatus.Done },
+                    new HabitRecord { Date = DateTime.Today.AddDays(-2), Status = CompletionStatus.Done },
+                    new HabitRecord { Date = DateTime.Today.AddDays(-1), Status = CompletionStatus.PartiallyDone },
+                    new HabitRecord { Date = DateTime.Today, Status = CompletionStatus.Done }
+                },
+                CurrentStreak = 3,
+                LongestStreak = 7,
+                LastCompletionDate = DateTime.Today,
+                CreatedDate = DateTime.Today.AddDays(-60)
+            });
+
+            Habits.Add(new Habit
+            {
+                Id = 3,
+                Name = "Физические упражнения",
+                Description = "15 минут зарядки каждое утро",
+                Periodicity = new HabitPeriodicity
+                {
+                    FrequencyType = Frequency.Daily,
+                },
+                CompletionRecords = new List<HabitRecord>
+                {
+                    new HabitRecord { Date = DateTime.Today.AddDays(-4), Status = CompletionStatus.NotDone },
+                    new HabitRecord { Date = DateTime.Today.AddDays(-3), Status = CompletionStatus.Done },
+                    new HabitRecord { Date = DateTime.Today.AddDays(-2), Status = CompletionStatus.PartiallyDone },
+                    new HabitRecord { Date = DateTime.Today.AddDays(-1), Status = CompletionStatus.Done }
+                },
+                CurrentStreak = 1,
+                LongestStreak = 4,
+                LastCompletionDate = DateTime.Today.AddDays(-1),
+                CreatedDate = DateTime.Today.AddDays(-15)
+            });
         }
     }
 }
